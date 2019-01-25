e2a214399ef8a9f9436a5f3cb8e4d968b8d385ba
Teemu Kääriä <teemu.kaaria@aalto.fi>
2019-01-25 20:46:34 +0200

    add pointseilijat

---
+++ pointseilijat/precache-manifest.a9bc1581fcf41c9dce1d34cbe981a35a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "128525307da3705de53d865345dcd116",
+    "url": "/pointseilijat/static/media/chevron-right.12852530.svg"
+  },
+  {
+    "revision": "439818dc561661c698ad1ed03443678e",
+    "url": "/pointseilijat/static/media/chevron-left.439818dc.svg"
+  },
+  {
+    "revision": "2b9fed2061ef038d6325",
+    "url": "/pointseilijat/static/js/runtime~main.2b9fed20.js"
+  },
+  {
+    "revision": "d7b1adbdd3f4d9fa196c",
+    "url": "/pointseilijat/static/js/main.d7b1adbd.chunk.js"
+  },
+  {
+    "revision": "741afdbbc9407809f816",
+    "url": "/pointseilijat/static/js/1.741afdbb.chunk.js"
+  },
+  {
+    "revision": "d7b1adbdd3f4d9fa196c",
+    "url": "/pointseilijat/static/css/main.3ff54b98.chunk.css"
+  },
+  {
+    "revision": "e96fb2cc63444e0362c90ee8fcb73dab",
+    "url": "/pointseilijat/index.html"
+  }
+];
